3dc7ffeaa3aa0467ad0abcecd28feb3460c29fa8
PiFaTos <52544202+PiFaTos@users.noreply.github.com>
2024-07-16 12:18:06 +0500

    121

--- menu.h
+++ menu.h
@@ -4,7 +4,7 @@
 #include <vector>
 #include <string>
 #include "graphics.h"
-
+//-------------------------------------
 using namespace std;
 
 extern int wx, wy;// Размеры окна
@@ -12,6 +12,7 @@ extern string filename;//Имя файла
 extern int dx, dy;//Размеры арта
 
 void draw_menu(); //Рисование меню
+
 string input_word(const int startX, const int startY, const int endX, const int endY); //Рисование имени файла или координат и их сохранение
 
 #endif  /* MENU */
