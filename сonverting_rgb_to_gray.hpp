e4f098cc1c4df5a8156d00fb20ec7bad9511d8a9
PiFaTos <52544202+PiFaTos@users.noreply.github.com>
2024-07-16 18:12:17 +0500

    Ошибки

--- сonverting_rgb_to_gray.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef ñonverting_rgb_to_gray_H
-#define ñonverting_rgb_to_gray_H
-#include<iostream>
-
-extern int r, g, b;
-double ñonverting_rgb_to_gray (int r, int g, int b);
-
-#endif  /* ñonverting_rgb_to_gray */
