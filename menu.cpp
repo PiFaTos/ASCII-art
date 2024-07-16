e2ea7f7b1370e753e44d7a352ce939f8e3e5fda0
quefz <168264170+quefz@users.noreply.github.com>
2024-07-16 14:02:31 +0500

    vse_ispravil

--- menu.cpp
+++ menu.cpp
@@ -3,7 +3,10 @@
 #include<graphics.h>
 #include <vector>
 #include <fstream>
+
 using namespace std;
+
+int xs1=20, xs2=120;
 
 //Рисование меню
 void draw_menu(){
@@ -11,54 +14,63 @@ void draw_menu(){
    bar(0 , 0, wx, wy);
    setfillstyle(1,WHITE);
    //Имя файла
-   bar(20, 60, 120, 80);
-   rectangle(20, 60, 120, 80);
-   outtextxy(20, 40, "Имя файла");
+   bar(xs1, 60, xs2, 80);
+   rectangle(xs1, 60, xs2, 80);
+   outtextxy(xs1, 40, "Имя файла");
    //Размеры арта
-   bar(20, 110, 60, 130);
-   rectangle(20, 110, 60, 130);
-   outtextxy(20, 90, "Ширина");
+   bar(xs1, 110, xs2/2, 130);
+   rectangle(xs1, 110, xs2/2, 130);
+   outtextxy(xs1, 90, "Ширина");
    
-   bar(80, 110, 120, 130);
-   rectangle(80, 110, 120, 130);
-   outtextxy(80, 90, "Высота");
+   bar(xs2-40, 110, xs2, 130);
+   rectangle(xs2-40, 110, xs2, 130);
+   outtextxy(xs2-40, 90, "Высота");
    //Сгенерировать
-   bar(20, 140, 120, 160);
-   rectangle(20, 140, 120, 160);
-   outtextxy(25, 145, "Сгенерировать");
+   bar(xs1, 140, xs2, 160);
+   rectangle(xs1, 140, xs2, 160);
+   outtextxy(xs1+5, 145, "Сгенерировать");
    //Очистить
-   bar(20, 180, 120, 200);
-   rectangle(20, 180, 120, 200);
-   outtextxy(25, 185, "Очистить");
+   bar(xs1, 180, xs2, 200);
+   rectangle(xs1, 180, xs2, 200);
+   outtextxy(xs1+5, 185, "Очистить");
+   //О программе
+   bar(xs1, 220, xs2, 240);
+   rectangle(xs1, 220, xs2, 240);
+   outtextxy(xs1+5, 225, "О программе");
 }
 
 //Проверка кнопки
-int prov_but(int x, int y){
-   if(x>=20 && x<=120 && y>=140 && y<=160) use_but(1);
-   if(x>=20 && x<=120 && y>=180 && y<=200) use_but(2);
+void prov_pole(int x, int y){
+   if(x>=xs1 && x<=xs2 && y>=140 && y<=160){use_but(1); return;}
+   if(x>=xs1 && x<=xs2 && y>=180 && y<=200){use_but(2); return;}
+   if(x>=xs1 && x<=xs2 && y>=60 && y<=80){filename=input_word(xs1, 60, xs2, 80); return;}
+   if(x>=xs1 && x<=xs2/2 && y>=110 && y<=130){dx=stoi(input_word(xs1, 110, xs2, 130)); return;}
+   if(x>=xs2-40 && x<=xs2 && y>=110 && y<=130){dx=stoi(input_word(xs1, 110, xs2, 130)); return;}
+   if(x>=xs1 && x<=xs2/2 && y>=220 && y<=240){about_programm(); return;}
    return;
 }
 
 //Использование кнопки
 void use_but(int i){
    switch(i){
-      case 1: generate();
-      case 2: clear_all();
+      case 1: generate(); break;
+      case 2: clear_all(); break;
    }
+   return;
 }
 
 //Рисование имени файла или искомого слова
 string input_word(const int startX, const int startY, const int endX, const int endY) {
    string a;
-   setbkcolor(NO_COLOR);
+   setbkcolor(WHITE);
    setfillstyle(1,COLOR(164, 245, 180));
    setcolor(BLACK);
-   int f;// Флаг
+   int f=1;// Флаг
    int ch;
    string word;
    int len = word.size();
    int x1 = startX, y1 = startY;
-   while (1) {
+   while (1){
       f=1;
       if(word.size()>50) f=0;
       ch = getch();
@@ -79,4 +91,8 @@ void clear_all(){
    draw_menu();
    dx=0, dy=0;
    filename="Noname.txt";
+}
+
+void about_programm(){
+   return;
 }
--- menu.hpp
+++ menu.hpp
@@ -14,9 +14,9 @@ extern int dx, dy;//Размеры арта
 
 void generate();//Создать изображение
 void clear_all();//Очистить значения
-int prov_but(int x, int y);//Проверка кнопки
+void prov_pole(int x, int y);//Проверка кнопки
 void use_but(int i);//Использование кнопки
 void draw_menu(); //Рисование меню
 string input_word(const int startX, const int startY, const int endX, const int endY); //Рисование имени файла или координат и их сохранение
-
+void about_programm(); //О программе
 #endif  /* MENU */
