caf02141ae3edbac3b02ceefae4e2b99b1d4caa5
quefz <168264170+quefz@users.noreply.github.com>
2024-07-16 12:17:21 +0500

    menu_end?

--- menu.cpp
+++ menu.cpp
@@ -6,10 +6,29 @@
 using namespace std;
 
 void draw_menu(){
-   setcolor(COLOR(0, 250, 154));
-   bar(0 ,0, wx, wy);
-   setcolor(WHITE);
-   bar()
+   setfillstyle(1,COLOR(164, 245, 180));
+   bar(0 , 0, wx, wy);
+   setfillstyle(1,WHITE);
+   //Имя файла
+   bar(20, 60, 120, 80);
+   rectangle(20, 60, 120, 80);
+   outtextxy(20, 40, "Имя файла");
+   //Размеры арта
+   bar(20, 110, 60, 130);
+   rectangle(20, 110, 60, 130);
+   outtextxy(20, 90, "Ширина");
+   
+   bar(80, 110, 120, 130);
+   rectangle(80, 110, 120, 130);
+   outtextxy(80, 90, "Высота");
+   //Сгенерировать
+   bar(20, 140, 120, 160);
+   rectangle(20, 140, 120, 160);
+   outtextxy(25, 145, "Сгенерировать");
+   //Очистить
+   bar(20, 180, 120, 200);
+   rectangle(20, 180, 120, 200);
+   outtextxy(25, 185, "Очистить");
 }
 
 //Рисование имени файла или искомого слова
@@ -23,9 +42,6 @@ string input_word(const int startX, const int startY, const int endX, const int 
    string word;
    int len = word.size();
    int x1 = startX, y1 = startY;
-   bar(startX - 5, startY - 5, endX + 5, endY + 5); // Очистка области для ввода
-   rectangle(startX, startY, endX, endY); // Рамка для ввода
-   outtextxy(620, 20, a.c_str());
    while (1) {
       f=1;
       if(word.size()>50) f=0;
@@ -35,11 +51,9 @@ string input_word(const int startX, const int startY, const int endX, const int 
       else if (ch == KEY_BACKSPACE && len > 0) word.erase(--len, 1);
       else if (ch > ' ' && ch <= 'z' && f && ch!='*'){word += ch; ++len;}
       // Вывод текущего ввода
-      bar(startX - 5, startY - 5, endX + 5, endY + 5);
+      bar(startX, startY, endX, endY);
       rectangle(startX, startY, endX, endY); 
-      outtextxy(x1+5, y1+10, word.c_str());
+      outtextxy(x1+5, y1+5, word.c_str());
    }
-   setfillstyle(1,COLOR(164, 245, 180));
-   pereris();
    return word;
 }
